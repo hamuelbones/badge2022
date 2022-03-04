a7ce1a66d46a36b8e51c3f3fdefc5db734b0c792
Samuel Jones <ssjones424@gmail.com>
2022-03-04 09:49:33 -0500

    Add micropython and pico sdk submodules.
    For micropython, we need it for building C extensions. And now that we're using submodules, then having the Pico SDK be a submodule is easier to track than the build system downloading it.

--- pico_sdk_import.cmake
+++ pico_sdk_import.cmake
@@ -51,12 +51,3 @@ get_filename_component(PICO_SDK_PATH "${PICO_SDK_PATH}" REALPATH BASE_DIR "${CMA
 if (NOT EXISTS ${PICO_SDK_PATH})
     message(FATAL_ERROR "Directory '${PICO_SDK_PATH}' not found")
 endif ()
-
-set(PICO_SDK_INIT_CMAKE_FILE ${PICO_SDK_PATH}/pico_sdk_init.cmake)
-if (NOT EXISTS ${PICO_SDK_INIT_CMAKE_FILE})
-    message(FATAL_ERROR "Directory '${PICO_SDK_PATH}' does not appear to contain the Raspberry Pi Pico SDK")
-endif ()
-
-set(PICO_SDK_PATH ${PICO_SDK_PATH} CACHE PATH "Path to the Raspberry Pi Pico SDK" FORCE)
-
-include(${PICO_SDK_INIT_CMAKE_FILE})
